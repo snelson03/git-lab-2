f43a3d1d6932acd8c51f13df6220ad3776a8099c
snelson <sn280121@ohio.edu>
2023-01-27 15:11:38 -0500

    Pushing to GitHub

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Seth Nelson
+ *        Date: 1/27/2023
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
